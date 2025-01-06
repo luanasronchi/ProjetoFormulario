e8760e41a13f7cc0905fbf1a241292773f538f08
Luana Siqueira Ronchi <luana.sronchi@gmail.com>
2025-01-06 16:06:01 -0300

    Add: Criação do projeto

---
+++ projeto-formulario/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <formulario/>
+</template>
+
+<script>
+import Formulario from "./components/Formulario.vue";
+
+export default {
+  name: "App",
+  components: {
+    Formulario,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
